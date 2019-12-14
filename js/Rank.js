a95e8a01bafb3a1550a68541e4bc3265e3c36672
Jaeyoon Song <jyo3on@gmail.com>
2019-12-15 05:13:54 +0900

    rank

--- js/Rank.js
+++ js/Rank.js
@@ -1,5 +1,5 @@
 var
-    url = "https://stats.oecd.org/SDMX-JSON/data/LFS_SEXAGE_I_R/AUS+AUT+BEL+CAN+CHL+CZE+DNK+EST+FIN+FRA+DEU+GRC+HUN+ISL+IRL+ISR+ITA+JPN+KOR+LUX+MEX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+TUR+GBR+USA.MW.900000.UR.A/all?startTime=2006&dimensionAtObservation=allDimensions",
+    url = "../test.json",
     width = 800,
     height = 280,
     margin = 1,
@@ -13,33 +13,43 @@ d3.json(url, function(error, json) {
 
     var
         ds = JSONstat(JSONstatUtils.fromSDMX(json)),
-        geo = ds.Dimension("COUNTRY"),
+        geo = ds.Dimension("CANDIDATE"),
         year = ds.Dimension("TIME_PERIOD").id,
-        start = String(year[year.length - 1]-11),
+        start = String(year[year.length - 1] - 11),
         atable = ds.toTable(
-            {type: 'arrobj', content: 'id'},
-            function(d){
+            {
+                type: 'arrobj', 
+                content: 'id'
+            },
+            function(d) {
                 if (d.TIME_PERIOD >= start){
-                    return {area: d.COUNTRY, year: d.TIME_PERIOD, value: d.value};
+                    return {
+                        area: d.CANDIDATE, 
+                        year: d.TIME_PERIOD, 
+                        value: d.value
+                    };
                 }
             }
         ),
-        max = atable.filter(function(d){return d.year===start;})
-        .sort(function(a,b) {
-            return b.value-a.value;
-        })
-        .map(function(d) {
-            return d.area;
-        })
+        max = atable
+            .filter(function(d){
+                return d.year === start;
+            })
+            .sort(function(a,b) {
+                return b.value-a.value;
+            })
+            .map(function(d) {
+                return d.area;
+            })
     ;
 
     atable.sort(function(a,b) {
-        return max.indexOf(a.area)-max.indexOf(b.area);
+        return max.indexOf(a.area) - max.indexOf(b.area);
     });
 
-    for (var i=year.indexOf(start), len=year.length; i<len; i++) {
-        var sel = (year[i] !== start) ? "" : ' class="selected"';
-        lis += "<li"+sel+">"+year[i]+"</li>";
+    for (var i = year.indexOf(start); i < year.length; i++) {
+        var sel = (year[i] !== start) ? "" : ' class = "selected"';
+        lis += "<li" + sel + ">" + year[i] + "</li>";
     }
 
     d3.select("#years").html(lis);
@@ -51,67 +61,88 @@ d3.json(url, function(error, json) {
         li = d3.selectAll("#years li")
     ;
 
-    li.on("click", function (d,i) {
-        li.classed("selected",false);
+    li.on("click", function (d, i) {
+        li.classed("selected", false);
         var sel = d3.select(this);
-        sel.classed("selected",true);
+        sel.classed("selected", true);
         Bars(atable, sel.text());
     });
 
     Bars(atable, start);
 
     function descr(d) {
-        return geo.Category(d.area).label+" "+Math.round(d.value*10)/10+"%";
+        return geo.Category(d.area).label + " " + Math.round(d.value * 10) / 10 + "%";
     }
 
-    function Bars(data,y) {
+    function Bars(data, y) {
         var arr=[], j=0;
-        for(var i=0; i<data.length;i++){
-            if(data[i].year==y){
-                arr.push({area: data[i].area, value: data[i].value, i: j++});
+        for(var i = 0; i < data.length; i++) {
+            if (data[i].year == y) {
+                arr.push({
+                    area: data[i].area, 
+                    value: data[i].value, 
+                    i: j++
+                });
             }
         }
         var rect = svg.selectAll("rect").data(arr);
         
         rect.enter().append("rect")
-            .attr("x", function(d,i){return vScale(i);})
-            .attr("y", function(d,i){return height-25 -  hScale(d.value);})
-            .attr("width",function(d,i){return ((width-2*margin)/arr.length)/2;})
-            .attr("height",function(d,i){return hScale(d.value);})
-            .attr("class","bar")
-            .on("mouseover", function(d,i){d3.select(this).classed("over", true);})
-            .on("mouseout", function(d,i){d3.select(this).classed("over",false);})
-            .on("click", function(d,i){
-                var sel=d3.select(this);
-                sel.classed("selected",!sel.classed("selected"));
+            .attr("x", function(d, i) {
+                return vScale(i);
             })
-            .append("title").text(function(d,i){return descr(d);})
+            .attr("y", function(d, i) {
+                return height-25 -  hScale(d.value);
+            })
+            .attr("width", function(d, i) {
+                return ((width-2*margin)/arr.length)/2;
+            })
+            .attr("height", function(d, i) {
+                return hScale(d.value);
+            })
+            .attr("class", "bar")
+            .on("mouseover", function(d, i) {
+                d3.select(this).classed("over", true);
+            })
+            .on("mouseout", function(d, i) {
+                d3.select(this).classed("over",false);
+            })
+            .on("click", function(d, i) {
+                var sel = d3.select(this);
+                sel.classed("selected", !sel.classed("selected"));
+            })
+            .append("title").text(function(d,i){
+                return descr(d);
+            })
             ;
 
         rect.transition().duration(2000)
             .attr("height",function(d,i){return hScale(d.value);})
-            .attr("y", function(d,i){return height-25 -  hScale(d.value);})
+            .attr("y", function(d,i){return height - 25 - hScale(d.value);})
             ;
 
         var titles=svg.selectAll("rect title").data(arr);
-        titles.text(function(d,i){
+        titles.text(function(d, i) {
             return descr(d);
         });
 
-        arr.sort(function(a,b){return b.value-a.value});
+        arr.sort(function(a, b) {
+            return b.value-a.value
+        });
 
+        // name
         var labels=svg.selectAll("text").data(max);
         labels.enter().append("text")
-            .attr("class",function(d,i){
+            .attr("class", function(d, i) {
                 return (arr[0].i!==i)? "text" : "text max";
             })
-            .attr("x", function(d,i){return vScale(i)+6;})
+            .attr("x", function(d, i) {return vScale(i) + 6;})
             .attr("y", 270)
             .attr("text-anchor","middle")
             .text(function(d,i){return d;})
             ;
         labels.transition().duration(0)
-            .attr("class",function(d,i){
+            .attr("class",function(d, i) {
                 return (arr[0].i!==i)? "text" : "text max";
             })
             ;
--- js/index.js
+++ js/index.js
@@ -1,41 +1,111 @@
-requirejs.config({
-    baseUrl: 'js',   
-    packages: ['LiveGantt', 'LiveGantt/model'],
-    paths: {
-      jquery: 'LiveGantt/lib/jquery',
-      d3: 'LiveGantt/lib/d3.v3.min',
-      bootstrap: 'LiveGantt/lib/bootstrap.min',
-      util: 'LiveGantt/util'
-    },
-    shim: {
-      bootstrap: {
-          deps: ['jquery']
-      }
+/**
+ * Created by Sallar Kaboli <sallar.kaboli@gmail.com>
+ * @sallar
+ * 
+ * Released under the MIT License.
+ * http://sallar.mit-license.org/
+ * 
+ * This document demonstrates three things:
+ * 
+ * - Creating a simple parallax effect on the content
+ * - Creating a Medium.com-style blur on scroll image
+ * - Getting scroll position using requestAnimationFrame for better performance
+ */
+
+$( document ).ready(function() {
+
+/**
+ * Cache
+ */
+var $content = $('#intro img'),
+    $blur    = $('#intro .overlay'),
+    $next    = $('#rank'),
+    wHeight  = $(window).height();
+
+$(window).on('resize', function(){
+  wHeight = $(window).height();
+});
+
+/**
+ * requestAnimationFrame Shim 
+ */
+window.requestAnimFrame = (function()
+{
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function( callback ) {
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
+
+/**
+ * Scroller
+ */
+function Scroller()
+{
+  this.latestKnownScrollY = 0;
+  this.ticking            = false;
+}
+
+Scroller.prototype = {
+  /**
+   * Initialize
+   */
+  init: function() {
+    window.addEventListener('scroll', this.onScroll.bind(this), false);
+  },
+
+  /**
+   * Capture Scroll
+   */
+  onScroll: function() {
+    this.latestKnownScrollY = window.scrollY;
+    this.requestTick();
+  },
+
+  /**
+   * Request a Tick
+   */
+  requestTick: function() {
+    if( !this.ticking ) {
+      window.requestAnimFrame(this.update.bind(this));
     }
-  });
-  
-  requirejs(['LiveGantt', 'jquery', 'd3']
-  , function(LiveGantt, $
-  ){
-    $.get('./schedule.json', function(json){
-      var schedule = LiveGantt.Model.Schedule.create(json);
-      var view = new LiveGantt.ScheduleView(schedule);
-      var width = 1280, height = 768;
-  
-      var svg = d3.select('svg').attr('width', width).attr('height', height);
-      svg.append('defs');
-      view
-        .width(width)
-        .height(height)
-        .render(svg, svg.append('g'));
-  
-      var $sortTypes = $('.sort-type');
-      $sortTypes.click(function(){
-        var $this = $(this),
-            sortType = parseInt($this.data('sort-type')),
-            mode = parseInt($this.data('mode'))
-            ;
-        view.update(mode, sortType);
-      });
+    this.ticking = true;
+  },
+
+  /**
+   * Update.
+   */
+  update: function() {
+    var currentScrollY = this.latestKnownScrollY;
+    this.ticking       = false;
+    
+    /**
+     * Do The Dirty Work Here
+     */
+    var slowScroll = currentScrollY
+      , blurScroll = currentScrollY * 2;
+    
+    $content.css({
+      'transform'         : 'translateY(-' + slowScroll + 'px)',
+      '-moz-transform'    : 'translateY(-' + slowScroll + 'px)',
+      '-webkit-transform' : 'translateY(-' + slowScroll + 'px)'
     });
-  });
+    
+    $blur.css({
+      'opacity' : blurScroll / wHeight
+    });
+
+    $next.css({
+      'opacity' : blurScroll / wHeight
+    })
+  }
+};
+
+/**
+ * Attach!
+ */
+  var scroller = new Scroller();  
+  scroller.init();
+});
